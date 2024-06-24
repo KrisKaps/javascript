369b5a7b21c77bd81334256fa3014ecfde1900cb
Christian Knist Pereira dos Santos <chriskpds@gmail.com>
2024-06-24 17:16:09 +0200

    Update script.js

--- exercicios/ex016/script.js
+++ exercicios/ex016/script.js
@@ -1,31 +1,33 @@
 function count() {
-    console.log('clicked count');
-    
-    let start = document.getElementById('istart').value;
-    let end = document.getElementById('iend').value;
-    let step = document.getElementById('istep').value;
-    
-    console.log('Start: ' + start);
-    console.log('End: ' + end);
-    console.log('Step: ' + step);
+  console.log("clicked count");
 
-    let counter = '';
+  let start = document.getElementById("istart").value;
+  let end = document.getElementById("iend").value;
+  let step = document.getElementById("istep").value;
 
-    if (start == ""){
-        alert('Start can\'t be empty!');
-    }
-    if (end == ""){
-        alert('End can\'t be empty!');
-    }
-    if (step == "" || step == '0'){
-        alert('Step can\'t be empty or "0"!');
-    }
-    if (parseInt(start) > parseInt(end)){
-        alert('Start can\'t be greater than End!');
-    }
+  console.log("Start: " + start);
+  console.log("End: " + end);
+  console.log("Step: " + step);
 
-    for (let i = parseInt(start); i <= parseInt(end); i += parseInt(step)) {
-        counter += i +', ';
-    }
-    document.getElementById('counter').innerHTML = counter;
-};
+  let counter = "";
+
+  if (start == "") {
+    alert("Start can't be empty!");
+  } else if (end == "") {
+    alert("End can't be empty!");
+  } else if (step == "" || step == "0" || parseInt(step) < 0) {
+    alert('Step can\'t be empty, "0" or smaller!');
+  } else if (parseInt(start) > parseInt(end)) {
+    alert("Start can't be greater than End!");
+  } else {
+      for (let i = parseInt(start); i <= parseInt(end); i += parseInt(step)) {
+        if (i == parseInt(end)) {
+            counter += i + '.';
+        } else {
+            counter += i + ', ';
+        }
+      }
+  }
+
+  document.getElementById("counter").innerHTML = counter;
+}
