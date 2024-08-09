2eb6a5237b8c5b34997f736ae4d4261597d9b0f3
Christian Knist Pereira dos Santos <chriskpds@gmail.com>
2024-08-09 14:06:32 +0200

    ex16 fix

--- exercicios/ex016/script2.js
+++ exercicios/ex016/script2.js
@@ -10,26 +10,36 @@ function count() {
     console.log('Step:', step, '-', typeof (step));
 
     let counter = '';
+    let cancount = true;
 
-    if (start == '') {
-        counter += 'Impossible to count without a Start.<br>';
-    }
-    if (end == '') {
-        counter += 'Impossible to count without an End.<br>';
-    }
-    if (parseInt(start) > parseInt(end)) {
-        counter += 'Impossible to count when Start higher than End.<br>';
-    }
     if (step == '' || step == "0" || parseInt(step) < 0) {
         alert('Impossible to count with this step value. Step value now equals 1');
         step = 1;
         document.getElementById('istep').value = 1;
-    } else {
+    }
+    if (start == '') {
+        counter += 'Impossible to count without a Start value.<br>';
+        cancount = false;
+    }
+    if (end == '') {
+        counter += 'Impossible to count without an End value.<br>';
+        cancount = false;
+    }
+    if (cancount) {
         counter += 'Counting:<br>';
-        for (let i = parseInt(start); i <= parseInt(end); i += parseInt(step)) {
-            counter += i + ' 👉🏽 ';
+        if (parseInt(start) > parseInt(end)) {
+            for (let i = parseInt(start); i >= parseInt(end); i -= parseInt(step)) {
+                // Uni-Code in JS: \u{1F449}
+                counter += i + ' 👉🏽 ';
+            }
+        } else {
+            for (let i = parseInt(start); i <= parseInt(end); i += parseInt(step)) {
+                counter += i + ' 👉🏽 ';
+            }
         }
+        // Uni-Code in JS: \u{1F3C1}
         counter += ' 🚩';
     }
+    
     document.getElementById('counter').innerHTML = counter;
 }
