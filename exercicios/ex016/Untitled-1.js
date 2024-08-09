9a0d1fb4d7c7ffd1fecfeedbf648df76bbba6e80
Christian Knist Pereira dos Santos <chriskpds@gmail.com>
2024-08-09 12:13:11 +0200

    untitled

---
+++ exercicios/ex016/Untitled-1.js
@@ -0,0 +1,36 @@
+function count() {
+    console.log("count was clicked");
+  
+    let start = document.getElementById("istart").value;
+    let end = document.getElementById("iend").value;
+    let step = document.getElementById("istep").value;
+  
+    console.log("Start: " + start + ' - ' + typeof(start));
+    console.log("End: " + end + ' - ' + typeof(end));
+    console.log("Step: " + step + ' - ' + typeof(step));
+  
+    let counter = "";
+  
+    if (start == "") {
+      alert("Start can't be empty!");
+    } else if (end == "") {
+      alert("End can't be empty!");
+    } else if (step == "" || step == "0" || parseInt(step) < 0) {
+      alert('Step can\'t be empty, "0" or smaller!');
+    } else if (parseInt(start) > parseInt(end)) {
+      alert("Start can't be greater than End!");
+      
+    } else {
+        for (let i = parseInt(start); i <= parseInt(end); i += parseInt(step)) {
+          if (i == parseInt(start)) {
+              counter += i;
+          } else {
+              counter += ', ' + i;
+          }
+        }
+        counter += '.';
+    }
+  
+    document.getElementById("counter").innerHTML = counter;
+}
+  
