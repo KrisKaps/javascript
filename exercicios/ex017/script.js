e6b19568d977fb5cbd90eb92d4c42bdc4e31dffc
Christian Knist Pereira dos Santos <chriskpds@gmail.com>
2024-08-09 15:47:32 +0200

    ex17 done

--- exercicios/ex017/script.js
+++ exercicios/ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá!');
+function timesTable() {
+    let n = document.getElementById('inum').value
+    let timestable = document.getElementById('itimes-table')
+
+    if (n.length == '') {
+        alert('Please enter a valid number.')
+    } else {
+        let num = parseInt(n)
+        let c = 1
+        timestable.innerHTML = '' // clear previous table before generating new one
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num * c}`
+            item.value = `tab${c}`
+            timestable.appendChild(item)
+            c++
+        }
+    }
+
+}
