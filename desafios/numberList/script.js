6712c72215340766cb9a0b17a6810354003fe9cd
Christian Knist Pereira dos Santos <chriskpds@gmail.com>
2024-08-27 17:04:51 +0200

    numberlist work

--- desafios/numberList/script.js
+++ desafios/numberList/script.js
@@ -1,24 +1,25 @@
 
 
 let numbers = []
-let finish = document.getElementById('ifinish')
+let finishtxt = document.getElementById('ifinish')
 
 function addNumber() {
     let n = document.getElementById('inum').value
+    console.log(n)
     let numberlist = document.getElementById('inumber-list')
     let num = parseInt(n)
 
-    if (n.length == '' || parseInt(n) < 1 || parseInt(n) > 100) {
+    if (n == '' || parseInt(n) < 1 || parseInt(n) > 100) {
         alert('Please enter a valid number.')
     } else if (numbers.indexOf(num) != -1) {
         alert('This number is already in the list.')
     } else {
-        finish.innerHTML = ''
+        finishtxt.innerHTML = ''
 
         numbers.push(num)
         console.log(numbers)
         let item = document.createElement('option')
-        item.text = `Valor ${num} foi adicionado`
+        item.text = `Number ${num} was added`
         item.value = `num${num}`
         numberlist.appendChild(item)
     }
@@ -28,10 +29,20 @@ function addNumber() {
 
 function finish() {
     
-    finish.innerHTML = ''
-
-    
     // timestable.innerHTML = '' // clear previous table before generating new one
+    finishtxt.innerHTML = ''
+
+    numbers.sort((a, b) => a - b) // sort numbers in descending order
+
+    finishtxt.innerHTML += `In total, we have ${numbers.length} added numbers.<br><br>`
+    
+    finishtxt.innerHTML += `The biggest added number is ${numbers[numbers.length-1]}.<br><br>`
+    
+    finishtxt.innerHTML += `The biggest added number is ${numbers[0]}.<br><br>`
+    
+    finishtxt.innerHTML += `Somando todos os valores, we have ${numbers[0]}.<br><br>`
+    
+    
         
     
 }
